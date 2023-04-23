26b1c17ae8a747cf8cb3083a45ac05fdcd01d2f2
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-04-23 17:45:53 +0530

    addd the layout

--- Frontend/Components/layout.js
+++ Frontend/Components/layout.js
@@ -5,13 +5,16 @@ import Hero from "./Hero/hero";
 
 import About from "./About/about";
 
+import Footer from "./Footer/footer";
+
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
       <Hero />
-      <About/>
+      {/* <About/> */}
       {children}
+      <Footer/>
     </>
   );
 }
