71a02d3179596c2f1abc3d8872b86b82d5db06c0
ankur <ankursingh91002@gmail.com>
2023-04-25 00:22:11 +0530

    Changes the structure of our webapp

---
+++ client/src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import About from '../components/About/About';
+import Navbar from "../components/Navbar/Navbar";
+import Footer from '../components/Footer/Footer';
+function AboutPage(){
+    return (
+        <>
+        <Navbar />
+        <About />
+        <Footer />
+        </>
+    )
+}
+
+export default AboutPage;
